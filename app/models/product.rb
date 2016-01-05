39d2abeb6a0a4ec44c2fa0c46a750ce9122d5a0b
Alexey Lazarev <siralexlazarev@gmail.com>
2016-01-05 12:28:49 +0700

    Valiations and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpeg|jpg|png)$}i,
+      # NOTE: not sure why it doesn't works in this ruby version
+      # ERROR: ArgumentError: The provided regular expression is using multiline anchors (^ or $),
+      # which may present a security risk. Did you mean to use \A and \z,
+      # or forgot to add the :multiline => true option?
+      multiline: true,
+      message: 'URL должен указывать на изображение.'
+  }
 end

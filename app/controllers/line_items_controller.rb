f44379d2f0efcc06a2c76c090e4530d5f94e2e8d
Alexey Lazarev <siralexlazarev@gmail.com>
2016-03-12 13:24:37 +0700

    added AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,15 +24,14 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    reset_counter
-
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id, product.price)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -60,8 +59,8 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to cart_url(current_cart), notice: 'Line item was successfully removed.' }
-      format.json { head :no_content }
+      format.html { redirect_to store_url }
+      format.json { head :ok }
     end
   end
 
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,6 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    increment_counter
-    @index_counter = get_counter
-
+    @cart = current_cart
   end
 end
